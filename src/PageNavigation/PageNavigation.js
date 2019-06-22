37f4776c3094baffa1a08dbc3f4d27b9f37aaab5
Christopher Williams <cw@chriswillia.ms>
2019-06-22 13:26:08 -0500

    Adds forward and back links to the PageNavigation though they currently take you to page 1. ¯\_(ツ)_/¯

--- src/PageNavigation/PageNavigation.js
+++ src/PageNavigation/PageNavigation.js
@@ -7,9 +7,11 @@ class PageNavigation extends React.Component {
             <nav>
                 <div className='page_list_container'>
                     <ul className='page_list'>
+                        <li className='page_list_number' key='-1' onClick={() => this.props.requestPageNumber(1)}><a className='page_list_link' href='#'>{'<<'}</a></li>
                         {this.props.pages.map((p, i) => {
                             return <li className='page_list_number' key={i} onClick={() => this.props.requestPageNumber(p)}><a className='page_list_link' href='#'>{p}</a></li>
                         })}
+                        <li className='page_list_number' key='-1' onClick={() => this.props.requestPageNumber(1)}><a className='page_list_link' href='#'>{'>>'}</a></li>
                     </ul>
                 </div>
             </nav>
